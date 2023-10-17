209dcf23053a6f47d4a8f27dfdb615b5c19bead6
Cheng/Dennis <115615421+thalassa111@users.noreply.github.com>
2023-10-17 15:11:01 +0200

    added menu example

---
+++ src/main/java/org/holidaymaker/menu/ActionAddCustomer.java
@@ -0,0 +1,8 @@
+package org.holidaymaker.menu;
+
+public class ActionAddCustomer implements MenuAction{
+    @Override
+    public void executeAction() {
+        System.out.println("adding customer...");
+    }
+}
--- src/main/java/org/holidaymaker/menu/MenuHandler.java
+++ src/main/java/org/holidaymaker/menu/MenuHandler.java
@@ -8,14 +8,15 @@ public class MenuHandler {
     Scanner scanner = new Scanner(System.in);
     private Map<Integer, MenuAction> menuOptions = new HashMap<>();
 
+    //add new meny things here
     public MenuHandler(){
-        /*menuOptions.put(1, new ActionNewGame());*/
+        menuOptions.put(1, new ActionAddCustomer());
     }
 
     public void displayMenu(){
         while(true){
             System.out.println("Menu");
-            System.out.println("1. hej");
+            System.out.println("1. add customer");
             int choice = scanner.nextInt();
 
             //will run the method executeAction depending on the choice picked earlier
