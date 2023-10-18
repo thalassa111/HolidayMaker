c205155502d70f3868f8f88cfaedf311519b4bd6
Cheng/Dennis <115615421+thalassa111@users.noreply.github.com>
2023-10-18 11:03:09 +0200

    added menu

---
+++ src/main/java/org/holidaymaker/menu/ActionTest.java
@@ -0,0 +1,8 @@
+package org.holidaymaker.menu;
+
+public class ActionTest implements MenuAction{
+    @Override
+    public void executeAction() {
+        System.out.println("testar action");
+    }
+}
