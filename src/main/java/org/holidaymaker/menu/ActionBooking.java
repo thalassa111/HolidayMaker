fb3de76dbc853267ae71a45ca3d444c816294c50
EdvinN1 <nedde14@gmail.com>
2023-10-25 15:42:52 +0200

    added travelpackage in booking

--- src/main/java/org/holidaymaker/menu/ActionBooking.java
+++ src/main/java/org/holidaymaker/menu/ActionBooking.java
@@ -6,39 +6,77 @@ import javax.sound.midi.Soundbank;
 import java.sql.Date;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ActionBooking implements MenuAction {
+    ArrayList<User> customers;
+    ArrayList<Activity> activities;
+    ArrayList<Accommodation> accommodations;
     @Override
     public void executeAction() {
+        Scanner scanner = new Scanner(System.in);
 
-        Database db = Database.getInstance();
+        System.out.println("Add Booking:");
+        System.out.println("1. Activities");
+        System.out.println("2. Travel Packages");
+        int choice = scanner.nextInt();
 
-        ArrayList<User> Customers;
-        ArrayList<Activity> Activities;
+        switch (choice) {
+            case 1:
+                activities = selectActivities();
+                customers = selectCustomers();
 
-        Activities = selectActivities();
-        Customers = selectCustomers();
+                if (!customers.isEmpty() && !activities.isEmpty()) {
+                    Date currentDate = new Date(System.currentTimeMillis());
+                    int newBookingId = Database.getInstance().createNewBooking(currentDate);
 
-        if (!Customers.isEmpty() && !Activities.isEmpty()) {
-            Date currentDate = new Date(System.currentTimeMillis());
-            int newBookingId = db.createNewBooking(currentDate);
+                    for (User customer : customers) {
+                        Database.getInstance().createNewBookingCustomer(customer.id(), newBookingId);
+                    }
 
-            for (User customer : Customers) {
-                db.createNewBookingCustomer(customer.id(), newBookingId);
-            }
-            int currentActivityPrice = 0;
-            for (Activity activity: Activities){
-                currentActivityPrice = Database.getInstance().getPriceOfActivityByID(activity.getId());
-                //adding price to the booking
-                Database.getInstance().addPriceToBookingByID(currentActivityPrice, newBookingId);
-                db.createNewBookingActivity(newBookingId, activity.getId());
-            }
-            addAccommodation(newBookingId, Activities);
+                    int currentActivityPrice = 0;
+                    for (Activity activity : activities) {
+                        currentActivityPrice = Database.getInstance().getPriceOfActivityByID(activity.getId());
+                        Database.getInstance().addPriceToBookingByID(currentActivityPrice, newBookingId);
+                        Database.getInstance().createNewBookingActivity(newBookingId, activity.getId());
+                    }
+                    addAccommodation(newBookingId, activities);
+                }
+                break;
+            case 2:
+                // Option 2 for Travel Packages
+                List<Activity> selectedPackageActivities = selectTravelPackage();
+                if (!selectedPackageActivities.isEmpty()) {
+                    ArrayList<User> packageCustomers = selectCustomers();
+
+                    if (!packageCustomers.isEmpty()) {
+                        Date currentDate = new Date(System.currentTimeMillis());
+                        int newPackageBookingId = Database.getInstance().createNewBooking(currentDate);
+
+                        for (User customer : packageCustomers) {
+                            Database.getInstance().createNewBookingCustomer(customer.id(), newPackageBookingId);
+                        }
+
+                        int packageActivityPrice = 0;
+                        for (Activity activity : selectedPackageActivities) {
+                            packageActivityPrice = Database.getInstance().getPriceOfActivityByID(activity.getId());
+                            Database.getInstance().addPriceToBookingByID(packageActivityPrice, newPackageBookingId);
+                            Database.getInstance().createNewBookingActivity(newPackageBookingId, activity.getId());
+                        }
+                        int packageAccommodationPrice = 0;
+                        for (Accommodation accommodation : accommodations){
+                            packageAccommodationPrice += accommodation.getPrice();
+                        }
+                        Database.getInstance().addPriceToBookingByID(packageAccommodationPrice, newPackageBookingId);
+                    }
+                }
+                break;
+            default:
+                System.out.println("Invalid choice. Please choose 1 for Activities or 2 for Travel Packages.");
+                break;
         }
-
     }
-
     public void addAccommodation(int bookingID, ArrayList<Activity> activities){
         Scanner scanner = new Scanner(System.in);
         int choice;
@@ -93,7 +131,6 @@ public class ActionBooking implements MenuAction {
             }
         }while(choice != 2);
     }
-
     private ArrayList<Activity> selectActivities() {
 
         Database db = Database.getInstance();
@@ -117,14 +154,11 @@ public class ActionBooking implements MenuAction {
                 System.out.println("No activity found for ID: " + userInput);
             }
         }
-
         System.out.println("Chosen Activity: ");
 
         for (Activity selectedActivity : selectedActivities) {
             System.out.println(selectedActivity);
         }
-
-
         return selectedActivities;
     }
 
@@ -152,7 +186,6 @@ public class ActionBooking implements MenuAction {
                 System.out.println("No Customer found for ID: " + userInput);
             }
         }
-
         System.out.println("Chosen Users: ");
         for (User selectedCustomer : selectedCustomers) {
             System.out.println(selectedCustomer);
@@ -160,7 +193,99 @@ public class ActionBooking implements MenuAction {
         }
         return selectedCustomers;
     }
+    public List<Activity> selectTravelPackage() {
+        List<Activity> selectedPackageActivities = new ArrayList<>();
 
+        System.out.println("Available Travel Packages:");
+
+        // Package 1
+        System.out.println("1. Package 1");
+        List<Activity> package1Activities = getActivitiesByIds(1, 2, 3);
+        displayPackageActivities(package1Activities);
+        selectedPackageActivities.addAll(package1Activities);
+        List<Accommodation> package1Accommodation = getAccommodationsById(1, 2, 3);
+        displayPackageAccommodations(package1Accommodation);
+
+        // Package 2
+        System.out.println("2. Package 2");
+        List<Activity> package2Activities = getActivitiesByIds(4, 5, 6);
+        displayPackageActivities(package2Activities);
+        List<Accommodation> package2Accommodation = getAccommodationsById(12, 13, 14);
+        displayPackageAccommodations(package2Accommodation);
+
+        // Package 3
+        System.out.println("3. Package 3");
+        List<Activity> package3Activities = getActivitiesByIds(8);
+        displayPackageActivities(package3Activities);
+        List<Accommodation> package3Accommodation = getAccommodationsById(17);
+        displayPackageAccommodations(package3Accommodation);
+
+        System.out.print("Choose a travel package by entering its number: ");
+        Scanner scanner = new Scanner(System.in);
+        int packageChoice = scanner.nextInt();
+
+        List<Activity> selectedPackage = new ArrayList<>();
+        if (packageChoice == 1) {
+            selectedPackage.addAll(package1Activities);
+            accommodations = (ArrayList<Accommodation>) package1Accommodation;
+        } else if (packageChoice == 2) {
+            selectedPackage.addAll(package2Activities);
+            accommodations = (ArrayList<Accommodation>) package2Accommodation;
+        } else if (packageChoice == 3) {
+            selectedPackage.addAll(package3Activities);
+            accommodations = (ArrayList<Accommodation>) package3Accommodation;
+        }
+
+        if (packageChoice >= 1 && packageChoice <= 3) {
+            return selectedPackage;
+        } else {
+            System.out.println("Invalid choice. No travel package selected.");
+            return new ArrayList<>();
+        }
+    }
+    private void displayPackageActivities(List<Activity> activities) {
+        for (Activity activity : activities) {
+            System.out.println("   ID: " + activity.getId() +
+                    " | Activity: " + activity.getActivityName() +
+                    " | Date: " + activity.getDate() +
+                    " | Location: " + activity.getLocation() +
+                    " | Price: " + activity.getPrice() +
+                    " | Description: " + activity.getDescription());
+        }
+    }
+    private void displayPackageAccommodations(List<Accommodation> accommodations) {
+        for (Accommodation accommodation : accommodations) {
+            System.out.println("   ID: " + accommodation.id() +
+                    " | Accommodation: " + accommodation.getAccomodation_name() +
+                    " | Date: " + accommodation.getAccomodation_date() +
+                    " | Location: " + accommodation.getLocation() +
+                    " | Price: " + accommodation.getPrice());
+        }
+    }
+    private List<Activity> getActivitiesByIds(int... ids) {
+        List<Activity> packageActivities = new ArrayList<>();
+
+        Database db = Database.getInstance();
+        for (int id : ids) {
+            List<Activity> foundActivities = db.findActivityById(id);
+            if (!foundActivities.isEmpty()) {
+                packageActivities.addAll(foundActivities);
+            }
+        }
+        return packageActivities;
+    }
+ private List<Accommodation> getAccommodationsById(int... ids){
+        List<Accommodation> packageAccommodations = new ArrayList<>();
+
+        Database db = Database.getInstance();
+        for (int id : ids) {
+            List<Accommodation> foundAccommodations = db.findAccommodationsById(id);
+            if (!foundAccommodations.isEmpty()) {
+                packageAccommodations.addAll(foundAccommodations);
+            }
+        }
+        return packageAccommodations;
+ }
     public void testData(Scanner tester) {
         System.out.print("Name: ");
         String name = tester.nextLine();
