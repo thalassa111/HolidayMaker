5122609b8231a1722360194edd5abdf2516d6bdc
Linus Rengbrandt <line.rengbrandt@gmail.com>
2023-10-25 11:19:28 +0200

    main and sub menu looks better

--- src/main/java/org/holidaymaker/menu/MenuHandler.java
+++ src/main/java/org/holidaymaker/menu/MenuHandler.java
@@ -8,8 +8,7 @@ public class MenuHandler {
     Scanner scanner = new Scanner(System.in);
     private Map<Integer, MenuAction> menuOptions = new HashMap<>();
 
-    //add new menu things here
-    public MenuHandler(){
+    public MenuHandler() {
         menuOptions.put(1, new ActionAddCustomer());
         menuOptions.put(2, new ActivitySubMenu());
         menuOptions.put(3, new ActionListCustomer());
@@ -17,15 +16,17 @@ public class MenuHandler {
         menuOptions.put(5, new ActionCheckBookings());
     }
 
-    public void displayMenu(){
-        while(true){
-            System.out.println("Menu");
-            System.out.println("1. add customer");
-            System.out.println("2. activities");
-            System.out.println("3. list customer");
+    public void displayMenu() {
+        while (true) {
+            System.out.println("\n#########################");
+            System.out.println("### HolidayMaker Menu ###");
+            System.out.println("#########################");
+            System.out.println("1. Add Customer");
+            System.out.println("2. Activities");
+            System.out.println("3. List Customers");
             System.out.println("4. Add Booking");
             System.out.println("5. Check Bookings");
-            System.out.println("0. Exit system");
+            System.out.println("0. Exit");
 
             try {
                 int choice = scanner.nextInt();
@@ -33,10 +34,10 @@ public class MenuHandler {
                 if (menuOptions.containsKey(choice)) {
                     menuOptions.get(choice).executeAction();
                 } else if (choice == 0) {
-                    System.out.println("Exiting system...");
+                    System.out.println("Exiting the system...");
                     System.exit(0);
                 } else {
-                    System.out.println("Invalid choice, pick again!");
+                    System.out.println("Invalid choice, please pick again.");
                 }
             } catch (java.util.InputMismatchException e) {
                 System.out.println("Invalid input. Please enter a valid number.");
@@ -44,8 +45,7 @@ public class MenuHandler {
             }
         }
     }
-    
-    // ActionSubMenu class for sub-menu
+
     private class ActivitySubMenu implements MenuAction {
         private Map<Integer, MenuAction> subMenuOptions = new HashMap<>();
 
@@ -58,11 +58,14 @@ public class MenuHandler {
         @Override
         public void executeAction() {
             while (true) {
-                System.out.println("Activities");
-                System.out.println("1. Add activity");
-                System.out.println("2. List activities");
-                System.out.println("3. Remove activity");
-                System.out.println("0. Back to main menu");
+                System.out.println("\n#####################");
+                System.out.println("### Activity Menu ###");
+                System.out.println("#####################");
+                System.out.println("1. Add Activity");
+                System.out.println("2. List Activities");
+                System.out.println("3. Remove Activity");
+                System.out.println("0. Back to Main Menu");
+
                 int choice = scanner.nextInt();
 
                 if (subMenuOptions.containsKey(choice)) {
@@ -70,7 +73,7 @@ public class MenuHandler {
                 } else if (choice == 0) {
                     break;
                 } else {
-                    System.out.println("Invalid choice, pick again!");
+                    System.out.println("Invalid choice, please pick again.");
                 }
             }
         }
